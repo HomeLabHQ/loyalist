0be3500f45c093e652ecfb17a9d89a240bbac62c
Dufran <zibertua@gmail.com>
2024-06-16 16:49:25 +0300

    Fix coverage for sonar

--- backend/core/tokens.py
+++ backend/core/tokens.py
@@ -1,9 +1,10 @@
 import six
+from authentication.models import User
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class _TokenGenFactory(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
+    def _make_hash_value(self, user: User, timestamp) -> str:
         return six.text_type(user.email) + six.text_type(timestamp) + six.text_type(user.is_active)
 
 
