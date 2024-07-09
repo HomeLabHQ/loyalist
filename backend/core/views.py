a2bf98858338c6b85d89dfedb4b098c1c3ff38cd
Dufran <zibertua@gmail.com>
2024-07-09 18:02:48 +0300

    Add file cleanup view

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,10 +2,11 @@ from django.core.files.storage import default_storage
 from django.core.files.uploadedfile import UploadedFile
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core.parsers import ImageUploadParser
+from core.parsers import CustomFileUploadParser, ImageUploadParser
 from core.serializers import ImageUploadSerializer
 from core.utils import preprocess_image
 
@@ -18,6 +19,7 @@ class ImageUploadView(APIView):
 
     """
 
+    permission_classes = (IsAuthenticated,)
     parser_classes = (ImageUploadParser,)
     serializer_class = ImageUploadSerializer
 
@@ -29,3 +31,38 @@ class ImageUploadView(APIView):
         url = default_storage.url(file.name)
         serializer = self.serializer_class({"name": file.name, "url": url})
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+class FileUploadView(APIView):
+    """Upload File to server"""
+
+    permission_classes = (IsAuthenticated,)
+    parser_classes = (CustomFileUploadParser,)
+    serializer_class = ImageUploadSerializer
+
+    @extend_schema(
+        request={
+            "content": {"format": "binary"},
+        },
+    )
+    def post(self, request, extension: str) -> Response:
+        _file: UploadedFile = request.data["file"]
+        with default_storage.open(_file.name, "wb") as f:
+            f.write(_file.read())
+        url = default_storage.url(_file.name)
+        serializer = self.serializer_class({"name": _file.name, "url": url})
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+class FileCleanUpView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    @extend_schema(
+        request=ImageUploadSerializer,
+        responses={status.HTTP_204_NO_CONTENT: None},
+    )
+    def post(self, request) -> Response:
+        filename = request.data.get("name")
+        if filename:
+            default_storage.delete(filename)
+        return Response(status=status.HTTP_204_NO_CONTENT)
