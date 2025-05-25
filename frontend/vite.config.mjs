f408bed08896d1e58096a1d12fc7c8660b336a24
oleksandr.korol <zibertua@gmail.com>
2025-05-25 09:07:08 +0300

    chores(deps): update frontend dependencies

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+import reactScan from '@react-scan/vite-plugin-react-scan';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), reactScan({})],
   server: {
     host: '0.0.0.0',
     port: 3000,
@@ -14,12 +15,13 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
   define: {
     'process.env': process.env,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
   },
 });
